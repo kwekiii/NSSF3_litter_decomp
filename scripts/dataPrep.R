3bcd7a0de9a0b395e55023e6ec734c030403f453
Chong Kwek Yan <kwekings@gmail.com>
2024-02-04 18:23:09 +0800

    Add files and update script

---
+++ scripts/dataPrep.R
@@ -0,0 +1,22 @@
+# Load libraries ----
+
+library(tidyverse)
+
+# Load data ----
+
+chns <- read.csv("./data/CHNS.csv", header=T)
+
+decomp <- read.csv("./data/decomp_final.csv", header = T) %>%
+  mutate(
+    Site = as.factor(Site),
+    Batch = as.factor(Batch),
+    Source_Cond = as.factor(Source_Cond),
+    Site_Cond = as.factor(Site_Cond),
+    LdryMass = log(dryMass),
+    LinitialMass = log(initialMass)
+  ) %>%
+  filter(numDaysField < 400)
+
+decomp$batch <- as.factor(substr(decomp$Batch, start=2, stop=2))
+decomp <- droplevels(decomp[-which(is.na(decomp$dryMass)),])
+decomp$months <- cut(decomp$numDaysField, breaks=c(-1,25,50,100,200,400), labels=c(0,1,2,4,8))
